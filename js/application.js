06fb8e6412ea213e90a0c8360b855f4b46270984
AyallaR <ayr569@gmail.com>
2022-11-15 15:55:50 +0200

    upload part1

---
+++ js/application.js
@@ -0,0 +1,7 @@
+function info() {  
+    alert (" You have come to the Info Page created by Ayalla R \n Welcome to the calculator.com \n The version is: 1.2 \n This site should help you calculate better... (not sure tho)");  
+ } 
+
+ function displayButtonInfo(value) {
+    alert(value);
+ }
